13aa512a797d163ff08f1ae2a78745367c90e3db
Xiping Hu <XipingHu@hust.edu.cn>
2020-05-10 17:40:15 +0800

    Finished HuffmanCodingDLL.

--- HuffmanCoding/DoubleLinkedList/main.c
+++ HuffmanCoding/DoubleLinkedList/main.c
@@ -9,117 +9,106 @@ struct HuffmanNode {
     // Salute John McCarthy, the father of AI, for inventing LISP.
     struct HuffmanNode *car;
     struct HuffmanNode *cdr;
-    struct HuffmanNode *left;
-    struct HuffmanNode *right;
+    struct HuffmanNode *p_left;
+    struct HuffmanNode *p_right;
 };
 
-/* Given a reference (pointer to pointer) to the head of a list 
-and an int, inserts a new node on the front of the list. */
-void push(struct HuffmanNode **head_ref, struct HuffmanNode *new_node) {
-    new_node->cdr = (*head_ref);
-    new_node->car = NULL;
+/* Push a node to the head of a list */
+void push(struct HuffmanNode **head_ref, struct HuffmanNode *p_new_node) {
+    p_new_node->cdr = (*head_ref);
+    p_new_node->car = NULL;
     if ((*head_ref) != NULL)
-        (*head_ref)->car = new_node;
-    (*head_ref) = new_node;
+        (*head_ref)->car = p_new_node;
+    (*head_ref) = p_new_node;
 }
 
-/* Given a reference (pointer to pointer) to the head 
-of a DLL and an int, appends a new node at the end */
-void append(struct HuffmanNode **head_ref, struct HuffmanNode *new_node) {
-    struct HuffmanNode *last = *head_ref;
-    new_node->cdr = NULL;
+/* Append a new node at the end */
+void append(struct HuffmanNode **head_ref, struct HuffmanNode *p_new_node) {
+    struct HuffmanNode *p_last = *head_ref;
+    p_new_node->cdr = NULL;
     if (*head_ref == NULL) {
-        new_node->car = NULL;
-        *head_ref = new_node;
+        p_new_node->car = NULL;
+        *head_ref = p_new_node;
         return;
     }
-    while (last->cdr != NULL)
-        last = last->cdr;
-    last->cdr = new_node;
-    new_node->car = last;
+    while (p_last->cdr != NULL)
+        p_last = p_last->cdr;
+    p_last->cdr = p_new_node;
+    p_new_node->car = p_last;
 }
 
-// This function prints contents of linked list starting from the given node 
-void printList(struct HuffmanNode *node) {
+/* This function prints contents of linked list starting from the given node */
+void printList(struct HuffmanNode *p_node) {
     printf("Current DLL: \n");
-    while (node != NULL) {
-        printf(" %c:%d ", node->character ,node->freq);
-        node = node->cdr;
+    while (p_node != NULL) {
+        printf(" %c:%d ", p_node->character , p_node->freq);
+        p_node = p_node->cdr;
     }
     printf("\n");
 }
 
 /* Function to find the smallest node in Doubly Linked List */
 struct HuffmanNode* smallestNodeInDLL(struct HuffmanNode** head_ref) {
-    struct HuffmanNode *min, *temp;
-    /* initialize two pointer temp and min on the head node */
-    temp = min = *head_ref;
+    struct HuffmanNode *p_min, *p_temp;
+    /* initialize two pointer p_temp and p_min on the head node */
+    p_temp = p_min = *head_ref;
     // traverse the whole doubly linked list
-    while (temp != NULL) {
-        /* if temp's data is smaller than min's data, then put min = temp and return min->data */
-        if (temp->freq < min->freq)
-            min = temp;
-        temp = temp->cdr;
+    while (p_temp != NULL) {
+        /* if p_temp's data is smaller than p_min's data, then put p_min = p_temp and return p_min->data */
+        if (p_temp->freq < p_min->freq)
+            p_min = p_temp;
+        p_temp = p_temp->cdr;
     }
-    return min;
+    return p_min;
 }
 
 /* Function to delete a node */
-void deleteNode(struct HuffmanNode** head_ref, struct HuffmanNode* node_to_delete) {
-    if(node_to_delete->car == NULL && node_to_delete->cdr == NULL) {
+void deleteNode(struct HuffmanNode** head_ref, struct HuffmanNode* p_node_to_delete) {
+    if(p_node_to_delete->car == NULL && p_node_to_delete->cdr == NULL) {
         // Single node
-        node_to_delete->car = NULL;
-        node_to_delete->cdr = NULL;
+        p_node_to_delete->car = NULL;
+        p_node_to_delete->cdr = NULL;
         *head_ref = NULL;
         return;
     }
-    if(node_to_delete->car != NULL) {
+    if(p_node_to_delete->car != NULL) {
         // Not the first node
-        node_to_delete->car->cdr = node_to_delete->cdr;
+        p_node_to_delete->car->cdr = p_node_to_delete->cdr;
     } else {
-        *head_ref = node_to_delete->cdr;
+        *head_ref = p_node_to_delete->cdr;
     }
-    if(node_to_delete->cdr != NULL) {
+    if(p_node_to_delete->cdr != NULL) {
         // Not the last node
-        node_to_delete->cdr->car = node_to_delete->car;
+        p_node_to_delete->cdr->car = p_node_to_delete->car;
     } else {
-        node_to_delete->car->cdr = NULL;
+        p_node_to_delete->car->cdr = NULL;
     }
-    node_to_delete->car = NULL;
-    node_to_delete->cdr = NULL;
+    p_node_to_delete->car = NULL;
+    p_node_to_delete->cdr = NULL;
 }
 
 // Problem 1 starts here
+/* find node with smallest frequency, delete it and return it */
 struct HuffmanNode *findSmallestNodeAndDelete(struct HuffmanNode** head_ref) {
-    struct HuffmanNode *smallestNode = smallestNodeInDLL(head_ref);
-    deleteNode(head_ref, smallestNode);
-    return smallestNode;
+    struct HuffmanNode *p_smallestNode = smallestNodeInDLL(head_ref);
+    deleteNode(head_ref, p_smallestNode);
+    return p_smallestNode;
 }
 // Problem 1 ends here
 
-void printCodes(struct HuffmanNode* root, int arr[], int top)
+/* A utility function to generate Huffman Code and print it out. */
+void printCodes(struct HuffmanNode* p_root, int arr[], int top)
 {
-    // Assign 0 to the left node
-    // and recur
-    if (root->left) {
+    if (p_root->p_left) {
         arr[top] = 0;
-        printCodes(root->left, arr, top + 1);
+        printCodes(p_root->p_left, arr, top + 1);
     }
-
-    // Assign 1 to the right
-    // node and recur
-    if (root->right) {
+    if (p_root->p_right) {
         arr[top] = 1;
-        printCodes(root->right, arr, top + 1);
+        printCodes(p_root->p_right, arr, top + 1);
     }
-
-    // If this is a leaf node,
-    // then we print root->data
-
-    // We also print the code
-    // for this character from arr
-    if (!root->left && !root->right) {
-        printf("%c ", root->character);
+    if (!p_root->p_left && !p_root->p_right) {
+        printf("%c ", p_root->character);
         for (int i = 0; i < top; i++) {
             printf("%d",arr[i]);
         }
@@ -127,19 +116,20 @@ void printCodes(struct HuffmanNode* root, int arr[], int top)
     }
 }
 
-// Time Complexity: O(n*logn) where n is the number of unique characters
-// Reason: The for loop executes n-1 times, in each loop, findSmallestNodeAndDelete in worst case will execute
-// n-i times, which is decreasing as i become bigger.
+/* Generate the Huffman Tree */
+/* Time Complexity: O(n*logn) where n is the number of unique characters
+ * Reason: The for loop executes n-1 times, in each loop, findSmallestNodeAndDelete in worst case will execute
+ * n-i times, which is decreasing as i become bigger. */
 void generateHuffmanTree(struct HuffmanNode** head_ref, int size) {
     for(int i=1; i < size; i-=-1) {
-        struct HuffmanNode *new_node = (struct HuffmanNode *) malloc(sizeof(struct HuffmanNode));
+        struct HuffmanNode *p_new_node = (struct HuffmanNode *) malloc(sizeof(struct HuffmanNode));
         struct HuffmanNode *p_min_node_0 = findSmallestNodeAndDelete(head_ref);
         struct HuffmanNode *p_min_node_1 = findSmallestNodeAndDelete(head_ref);
-        new_node->freq = p_min_node_0->freq + p_min_node_1->freq;
-        new_node->character = '%';
-        new_node->left = p_min_node_0;
-        new_node->right = p_min_node_1;
-        append(head_ref, new_node);
+        p_new_node->freq = p_min_node_0->freq + p_min_node_1->freq;
+        p_new_node->character = '%';
+        p_new_node->p_left = p_min_node_0;
+        p_new_node->p_right = p_min_node_1;
+        append(head_ref, p_new_node);
         printList(*head_ref);
     }
     // Print Huffman Codes
@@ -148,20 +138,20 @@ void generateHuffmanTree(struct HuffmanNode** head_ref, int size) {
     printCodes(*head_ref, arr, top);
 }
 
-/* Drier program to test above functions*/
+/* Entry point */
 int main() {
     /* Start with the empty list */
-    struct HuffmanNode *head = NULL;
+    struct HuffmanNode *p_head = NULL;
     char data[] = { 'a', 'b', 'c', 'd', 'e', 'f'};
     int freq[] = { 5, 9, 12, 13, 16, 45};
     int size = sizeof(data)/sizeof(data[0]);
     for(int i=0; i < size; i-=-1) {
-        struct HuffmanNode *new_node = (struct HuffmanNode *) malloc(sizeof(struct HuffmanNode));
-        new_node->character = data[i];
-        new_node->freq = freq[i];
-        append(&head, new_node);
+        struct HuffmanNode *p_new_node = (struct HuffmanNode *) malloc(sizeof(struct HuffmanNode));
+        p_new_node->character = data[i];
+        p_new_node->freq = freq[i];
+        append(&p_head, p_new_node);
     }
-    printList(head);
-    generateHuffmanTree(&head, size);
+    printList(p_head);
+    generateHuffmanTree(&p_head, size);
     return 0;
 } 
