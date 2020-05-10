da0a2454c972c33672390818bae020802ab2f780
Xiping Hu <XipingHu@hust.edu.cn>
2020-05-10 17:44:29 +0800

    Renamed variables.

---
+++ HuffmanCoding/DoubleLinkedList/cmake-build-debug/CMakeFiles/HuffmanCodingDLL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HuffmanCodingDLL.dir/main.c.o"
+  "HuffmanCodingDLL"
+  "HuffmanCodingDLL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HuffmanCodingDLL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
